5bf78f046f209f72c9c9e40e59d8565166b83c40
nomanr <nomrafique@gmail.com>
2025-05-04 19:29:20 +0100

    update shake x and y

--- animate-compose/src/commonMain/kotlin/com/nomanr/animate/compose/presets/attentionseekers/ShakeX.kt
+++ animate-compose/src/commonMain/kotlin/com/nomanr/animate/compose/presets/attentionseekers/ShakeX.kt
@@ -16,24 +16,76 @@ class ShakeX(
     private val ease = FastOutSlowInEasing
 
     private val keyframes = listOf(
-        shake(0f, 0.1f, 0f, -offsetX),
-        shake(0.1f, 0.2f, -offsetX, offsetX),
-        shake(0.2f, 0.3f, offsetX, -offsetX),
-        shake(0.3f, 0.4f, -offsetX, offsetX),
-        shake(0.4f, 0.5f, offsetX, -offsetX),
-        shake(0.5f, 0.6f, -offsetX, offsetX),
-        shake(0.6f, 0.7f, offsetX, -offsetX),
-        shake(0.7f, 0.8f, -offsetX, offsetX),
-        shake(0.8f, 0.9f, offsetX, -offsetX),
-        shake(0.9f, 1f, -offsetX, 0f)
-    )
-
-    private fun shake(start: Float, end: Float, from: Float, to: Float) = Keyframe.Segment(
-        start = start,
-        end = end,
-        from = TransformProperties(translationX = from),
-        to = TransformProperties(translationX = to),
-        easing = ease
+        Keyframe.Segment(
+            start = 0f,
+            end = 0.1f,
+            from = TransformProperties(translationX = 0f),
+            to = TransformProperties(translationX = -offsetX),
+            easing = ease
+        ),
+        Keyframe.Segment(
+            start = 0.1f,
+            end = 0.2f,
+            from = TransformProperties(translationX = -offsetX),
+            to = TransformProperties(translationX = offsetX),
+            easing = ease
+        ),
+        Keyframe.Segment(
+            start = 0.2f,
+            end = 0.3f,
+            from = TransformProperties(translationX = offsetX),
+            to = TransformProperties(translationX = -offsetX),
+            easing = ease
+        ),
+        Keyframe.Segment(
+            start = 0.3f,
+            end = 0.4f,
+            from = TransformProperties(translationX = -offsetX),
+            to = TransformProperties(translationX = offsetX),
+            easing = ease
+        ),
+        Keyframe.Segment(
+            start = 0.4f,
+            end = 0.5f,
+            from = TransformProperties(translationX = offsetX),
+            to = TransformProperties(translationX = -offsetX),
+            easing = ease
+        ),
+        Keyframe.Segment(
+            start = 0.5f,
+            end = 0.6f,
+            from = TransformProperties(translationX = -offsetX),
+            to = TransformProperties(translationX = offsetX),
+            easing = ease
+        ),
+        Keyframe.Segment(
+            start = 0.6f,
+            end = 0.7f,
+            from = TransformProperties(translationX = offsetX),
+            to = TransformProperties(translationX = -offsetX),
+            easing = ease
+        ),
+        Keyframe.Segment(
+            start = 0.7f,
+            end = 0.8f,
+            from = TransformProperties(translationX = -offsetX),
+            to = TransformProperties(translationX = offsetX),
+            easing = ease
+        ),
+        Keyframe.Segment(
+            start = 0.8f,
+            end = 0.9f,
+            from = TransformProperties(translationX = offsetX),
+            to = TransformProperties(translationX = -offsetX),
+            easing = ease
+        ),
+        Keyframe.Segment(
+            start = 0.9f,
+            end = 1f,
+            from = TransformProperties(translationX = -offsetX),
+            to = TransformProperties(translationX = 0f),
+            easing = ease
+        )
     )
 
     @Composable
